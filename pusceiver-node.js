f98de09457ae4b70ff026675136743a29479df1b
Hiroshi Saito <hiroshi3110@gmail.com>
2013-03-02 16:33:38 +0900

    Use firebase path /user/:user_id/items instead of root

--- pusceiver-node.js
+++ pusceiver-node.js
@@ -1,26 +1,22 @@
 var util = require('util');
+var url = require('url');
 
 var Firebase = require('./firebase-node');
-var myRootRef = new Firebase('https://pusceiver.firebaseIO.com/');
+var rootRef = new Firebase('https://pusceiver.firebaseIO.com/');
 var FirebaseTokenGenerator = require("firebase-token-generator");
 var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_APP_SECRET);
 var token = tokenGenerator.createToken({}, {admin: true, debug: true});
-myRootRef.auth(token);
 
 var Kaiseki = require('kaiseki');
 var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY);
 
-
-util.debug("pusceiver-node will start");
-// myRootRef.push('Hello from heroku!');
-myRootRef.startAt(Date.now()).on('child_added', function(snapshot) {
-    util.debug(snapshot.getPriority() + ":" + snapshot.val());
+function push_notification(itemSnapshot) {
     var notification = {
         //channels: [''],
         where: { "deviceType": "ios" },
         data: {
-            p: "/" + snapshot.name(),
-            alert: snapshot.val()
+            p: url.parse(itemSnapshot.ref().toString()).path,
+            alert: itemSnapshot.val()
         }
     };
     kaiseki.sendPushNotification(notification, function(err, res, body, success) {
@@ -29,5 +25,13 @@ myRootRef.startAt(Date.now()).on('child_added', function(snapshot) {
         } else {
             console.log('Could not send push notification:', err, body);
         }
+    });
+}
+
+util.debug("pusceiver-node will start");
+rootRef.auth(token);
+rootRef.child("users").on('child_added', function(userSnapshot) {
+    userSnapshot.ref().child('items').startAt(Date.now()).on('child_added', function(itemSnapshot) {
+        push_notification(itemSnapshot);
     });
 });
